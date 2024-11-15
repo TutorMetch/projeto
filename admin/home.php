21aae78eb4570fd7a8de5321ec5bea588a351df3
pablogfgdgf <pbraz0460@gmail.com>
2024-11-14 21:39:03 -0300

    sds

--- admin/home.php
+++ admin/home.php
@@ -55,6 +55,9 @@
                           <li class="nav-item">
                             <a class="nav-link" href="?pg=menu">menu</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="?pg=cursos">cursos</a>
+                        </li>
                     </ul>
                 </div>
             </nav>
